690a28347587ebf8c9b9d36625439e343df98a07
OVO <lzx213@sina.com>
2024-09-19 09:48:32 -0600

    change the culling mask when change the view for fire vfx

--- Assets/Scripts/Zexuan/SwitchCamera.cs
+++ Assets/Scripts/Zexuan/SwitchCamera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class SwitchCamera : MonoBehaviour
 {
+    public GameObject mainCamera;
     public GameObject camera1;
     public GameObject camera2;
     public GameObject minimapCamera1;
@@ -13,17 +14,17 @@ public class SwitchCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F))
         {
             camera1.SetActive(!camera1.activeSelf);
             camera2.SetActive(!camera2.activeSelf);
-            
+
             camera2.transform.position = camrera2OriginPos.transform.position;
             camera2.transform.rotation = camrera2OriginPos.transform.rotation;
 
@@ -33,14 +34,26 @@ public class SwitchCamera : MonoBehaviour
             GameManager.Instance.isThirdPesronView = !GameManager.Instance.isThirdPesronView;
             GameManager.Instance.isWorldView = !GameManager.Instance.isWorldView;
 
-            if(GameManager.Instance.isWorldView)
+            if (GameManager.Instance.isWorldView)
             {
                 GameManager.Instance.player.transform.SetParent(GameManager.Instance.planet.transform);
             }
-            else if(GameManager.Instance.isThirdPesronView)
+            else if (GameManager.Instance.isThirdPesronView)
             {
                 GameManager.Instance.player.transform.SetParent(null);
-            }   
+            }
+
+            //change culling mask of main camera
+            if (GameManager.Instance.isThirdPesronView)
+            {
+                mainCamera.GetComponent<Camera>().cullingMask = -1;
+                mainCamera.GetComponent<Camera>().cullingMask &= ~(1 << LayerMask.NameToLayer("PlayerTag"));
+            }
+            else if (GameManager.Instance.isWorldView)
+            {
+                mainCamera.GetComponent<Camera>().cullingMask = -1;
+                mainCamera.GetComponent<Camera>().cullingMask &= ~((1 << LayerMask.NameToLayer("PlayerTag")) | (1 << LayerMask.NameToLayer("Fire")));
+            }
         }
     }
 
