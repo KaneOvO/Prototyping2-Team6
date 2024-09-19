ef31071dc69e9eb8db24ecefd48889829de47c88
OVO <lzx213@sina.com>
2024-09-19 10:46:55 -0600

    add preview when planting tree

--- Assets/Scripts/Zexuan/PlantingTool.cs
+++ Assets/Scripts/Zexuan/PlantingTool.cs
@@ -7,6 +7,9 @@ public class PlantingTool : MonoBehaviour
 {
     public Tools tools;
     public GameObject treePrefab;
+    public GameObject treePreviewPrefab;
+    private GameObject currentPreviewTree;
+    private bool isPreviewing = false;
 
     void Start()
     {
@@ -17,11 +20,69 @@ public class PlantingTool : MonoBehaviour
     {
         if (tools.isPlantingTool)
         {
-            if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+            if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject())
             {
-                PlantingTreeOrDestoryTree();
-                
+                ShowTreePreview();
             }
+            else if (Input.GetMouseButtonUp(0) && !EventSystem.current.IsPointerOverGameObject() && CheckCanPlanting(currentPreviewTree))
+            {
+                RemoveTreePreview();
+                PlantingTreeOrDestoryTree();
+                Debug.Log("PlantingTree");
+            }
+            else if (Input.GetMouseButtonUp(0) && !CheckCanPlanting(currentPreviewTree))
+            {
+                RemoveTreePreview();
+                Debug.Log("RemoveTreePreview");
+            }
+            else if(!Input.GetMouseButton(0) && isPreviewing)
+            {
+                RemoveTreePreview();
+                Debug.Log("RemoveTreePreview");
+            }
+        }
+        else
+        {
+            if (isPreviewing)
+            {
+                RemoveTreePreview();
+            }
+        }
+    }
+
+    void ShowTreePreview()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            
+            if (hit.collider.CompareTag("Planet"))
+            {
+                if (currentPreviewTree == null)
+                {
+                    
+                    currentPreviewTree = Instantiate(treePreviewPrefab, hit.point, Quaternion.identity);
+                    isPreviewing = true;
+                }
+                else
+                {
+                    
+                    currentPreviewTree.transform.position = hit.point;
+                    currentPreviewTree.transform.up = hit.normal;
+                }
+            }
+        }
+    }
+
+    void RemoveTreePreview()
+    {
+        if (currentPreviewTree != null)
+        {
+            Destroy(currentPreviewTree);
+            currentPreviewTree = null;
+            isPreviewing = false;
         }
     }
 
@@ -32,24 +93,24 @@ public class PlantingTool : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-
+            Debug.Log(hit.collider.tag);
             if (hit.collider.CompareTag("Planet"))
             {
                 GameObject tree = Instantiate(treePrefab, hit.point, Quaternion.identity);
-
                 tree.transform.up = hit.normal;
-                //set parent to planet
                 tree.transform.SetParent(hit.collider.transform.Find("Trees"));
             }
             else if (hit.collider.CompareTag("Tree"))
             {
-                // if(hit.collider.GetComponent<Tree>().isBurned)
-                // {
-                //     Destroy(hit.collider.gameObject);
-                // }
-                
                 Destroy(hit.collider.gameObject);
+                Debug.Log("DestroyTree");
             }
         }
     }
+
+    bool CheckCanPlanting(GameObject tree)
+    {
+        return tree.transform.Find("PlantingCheck").GetComponent<PlantingCheck>().canPlanting;
+    }
+
 }
