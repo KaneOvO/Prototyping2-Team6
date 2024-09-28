5e2396b43004117a1f219f1a5d5dc5377ec1a246
Ray819038825 <u1537043@utah.edu>
2024-09-28 15:34:05 -0600

    Fixing bug

--- Assets/Textures/EdgeDetectionPostProessEffect.cs
+++ Assets/Textures/EdgeDetectionPostProessEffect.cs
@@ -1,21 +1,21 @@
-﻿using UnityEngine;
+﻿//using UnityEngine;
 
-[RequireComponent(typeof(Camera))]
-public class EdgeDetectionPostProcessEffect : MonoBehaviour
-{
-    public Material postProcessMaterial;
+//[RequireComponent(typeof(Camera))]
+//public class EdgeDetectionPostProcessEffect : MonoBehaviour
+//{
+//    public Material postProcessMaterial;
 
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        if (postProcessMaterial != null)
-        {
-            // 使用我们创建的材质处理屏幕图像
-            Graphics.Blit(source, destination, postProcessMaterial);
-        }
-        else
-        {
-            // 如果没有指定材质，直接显示原图像
-            Graphics.Blit(source, destination);
-        }
-    }
-}
+//    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+//    {
+//        if (postProcessMaterial != null)
+//        {
+//            // 使用我们创建的材质处理屏幕图像
+//            Graphics.Blit(source, destination, postProcessMaterial);
+//        }
+//        else
+//        {
+//            // 如果没有指定材质，直接显示原图像
+//            Graphics.Blit(source, destination);
+//        }
+//    }
+//}
